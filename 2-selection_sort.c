713e59658ed67913d46f02bdaa3cea444248cbef
MikeBilbee <cmbilbee@gmail.com>
2023-04-22 22:08:26 -0500

    2.2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,44 +1,56 @@
 #include "sort.h"
 
 /**
-* selection_sort- sorts an array of integers
-* in ascending order using selection sort algorithm
-* @array: an array of integers
-* @size: size of the array of integers
-* Return: Nothing
-*/
+ * array_swap - swaps two integers in an array
+ * @array: array to be sorted
+ * @a: index of first integer
+ * @b: index of second integer
+ *
+ * Return: Void
+ */
+
+void array_swap(int *array, int a, int b)
+{
+	int temp;
+
+	temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
+}
+
+/**
+ * selection_sort - implementation of selection sort algorithm
+ * @array: provided array of integers to sort
+ * @size: size of provided array
+ *
+ * Return: Void
+ */
 
 void selection_sort(int *array, size_t size)
 {
-size_t i = 0, j = 0;
-size_t k = 0;
-int tmp = 0;
+	unsigned int i, j, index;
+	int temp, swap = 0;
 
-if (size < 2)
-{
-return;
-}
+	if (size < 2)
+		return;
 
-while (i < size)
-{
-k = i;
-
-for (j = i + 1; j < size; j++)
-{
-if (array[k] > array[j])
-{
-k = j;
-}
-}
-
-if (k != i)
-{
-tmp = array[i];
-array[i] = array[k];
-array[k] = tmp;
-print_array(array, size);
-}
-
-i++;
-}
+	for (i = 0; i < size - 1; i++)
+	{
+		temp = array[i];
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < temp)
+			{
+				temp = array[j];
+				index = j;
+				swap = 1;
+			}
+			if ((swap == 1) && (j == size - 1))
+			{
+				array_swap(array, i, index);
+				print_array(array, size);
+			}
+		}
+		swap = 0;
+	}
 }
